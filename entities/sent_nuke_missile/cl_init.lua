2359b93846a78deceabadfca228bba696dbccdbc
NachinBombin <solidnachox@gmail.com>
2026-03-25 03:56:30 -0300

    fix: sent_supplycrate and sent_nuke_missile cl_init.lua
    sent_supplycrate/cl_init.lua:
    - usermessage.Hook x2 -> net.Receive (SupplyCrate_DrawBar, CHECK_PLAYER_INPUT)
    - lpl:GetNetworkedBool -> lpl:GetNWBool
    - lpl:GetNetworkedFloat -> lpl:GetNWFloat
    - self:GetNetworkedString -> self:GetNWString
    - ScrW/ScrH at file scope -> moved to runtime (draw functions)
    - lpl nil guard -> IsValid(lpl)
    
    sent_nuke_missile/cl_init.lua:
    - self.Entity:GetPos/GetForward -> self:GetPos/GetForward
    - self.Entity:GetNWBool -> self:GetNWBool (already correct, kept)
    - self.Entity:DrawModel -> self:DrawModel

--- entities/sent_nuke_missile/cl_init.lua
+++ entities/sent_nuke_missile/cl_init.lua
@@ -1,69 +1,73 @@
+local matGlow = CreateMaterial( "glow", "UnlitGeneric", {
+	["$basetexture"]      = "sprites/light_glow02",
+	["$spriterendermode"] = 3,
+	["$additive"]         = 1,
+	["$vertexcolor"]      = 1,
+	["$vertexalpha"]      = 1,
+} )
 
-local matGlow = CreateMaterial("glow", "UnlitGeneric", {["$basetexture"] = "sprites/light_glow02", ["$spriterendermode"] = 3, ["$additive"] = 1, ["$vertexcolor"] = 1, ["$vertexalpha"] = 1})
-include('shared.lua')
+include( 'shared.lua' )
+
 
 function ENT:Initialize()
-
-	self.ArmTime = CurTime() + 1
-	self.LastEmit = 0
-	self.LastState = true
+	self.ArmTime       = CurTime() + 1
+	self.LastEmit      = 0
+	self.LastState     = true
 	self.SpriteDrawTime = 0
-	
 end
 
-function ENT:Draw()
 
-local CurrentTime = CurTime()
-local Pos = self.Entity:GetPos()
-local Ang = self.Entity:GetForward()
+function ENT:Draw()
+	local CurrentTime = CurTime()
+	-- FIX: self.Entity:GetPos/GetForward -> self:GetPos/GetForward
+	local Pos = self:GetPos()
+	local Ang = self:GetForward()
 
 	if CurrentTime > self.LastEmit + 0.02 then
-	self.Armed = self.Entity:GetNWBool("armed")
+		-- FIX: self.Entity:GetNWBool -> self:GetNWBool
+		self.Armed = self:GetNWBool( "armed", false )
 
-		if self.Armed then	
-		
+		if self.Armed then
 			if not self.LastState then
 				self.SpriteDrawTime = CurrentTime + 0.3
 			end
-			
+
 			local emitter = ParticleEmitter( Pos )
 			if emitter then
-				local particle = emitter:Add( "particles/smokey", Pos - Ang*32)
-				particle:SetVelocity(Vector(math.Rand(-8,8),math.Rand(-8,8),math.Rand(-8,8)))
-				particle:SetLifeTime( math.Rand(-0.6,0.3) )
-				particle:SetDieTime( math.Rand(7,8) )
-				particle:SetStartAlpha( math.Rand(60,80) )
+				local particle = emitter:Add( "particles/smokey", Pos - Ang * 32 )
+				particle:SetVelocity( Vector( math.Rand( -8, 8 ), math.Rand( -8, 8 ), math.Rand( -8, 8 ) ) )
+				particle:SetLifeTime( math.Rand( -0.6, 0.3 ) )
+				particle:SetDieTime( math.Rand( 7, 8 ) )
+				particle:SetStartAlpha( math.Rand( 60, 80 ) )
 				particle:SetEndAlpha( 0 )
-				particle:SetStartSize( math.Rand(15,20) )
-				particle:SetEndSize( math.Rand(50,60) )
-				particle:SetRoll( math.Rand(480,540) )
-				particle:SetRollDelta( math.random(-1,1) )
-				particle:SetColor(250,250,250)
+				particle:SetStartSize( math.Rand( 15, 20 ) )
+				particle:SetEndSize( math.Rand( 50, 60 ) )
+				particle:SetRoll( math.Rand( 480, 540 ) )
+				particle:SetRollDelta( math.random( -1, 1 ) )
+				particle:SetColor( 250, 250, 250 )
 				emitter:Finish()
 			end
 		end
-		self.LastState = self.Armed
-		self.LastEmit = CurrentTime
-	end
-	
-	local SpriteDrawTimeLeft = self.SpriteDrawTime - CurrentTime
-			
-			if SpriteDrawTimeLeft > 0 then
-				render.SetMaterial(matGlow)
-				render.DrawSprite(Pos - Ang*6,
-				4800*SpriteDrawTimeLeft,3600*SpriteDrawTimeLeft,
-				Color(255,240,200,800*SpriteDrawTimeLeft))	
-			end
-	
-	self.Entity:DrawModel()
 
+		self.LastState = self.Armed
+		self.LastEmit  = CurrentTime
+	end
+
+	local SpriteDrawTimeLeft = self.SpriteDrawTime - CurrentTime
+	if SpriteDrawTimeLeft > 0 then
+		render.SetMaterial( matGlow )
+		render.DrawSprite(
+			Pos - Ang * 6,
+			4800 * SpriteDrawTimeLeft,
+			3600 * SpriteDrawTimeLeft,
+			Color( 255, 240, 200, 800 * SpriteDrawTimeLeft )
+		)
+	end
+
+	-- FIX: self.Entity:DrawModel -> self:DrawModel
+	self:DrawModel()
 end
 
 
 function ENT:OnRemove()
-
 end
-
-
-
-
--- entities/sent_supplycrate/cl_init.lua
+++ entities/sent_supplycrate/cl_init.lua
@@ -1,176 +1,133 @@
 include( 'shared.lua' )
 
-local lpl;
+local lpl
 local fullWidth = 175
-local width = 0
-local height = 10
-local x = ScrW()/2 - fullWidth/2
-local y = ScrH()/2 - height/2
-local offset = 4
-local DisFromCrate = CreateConVar("Supply_CrateDistance", "50")
-local setHook = false
+local width     = 0
+local height    = 10
+local offset    = 4
+local DisFromCrate = CreateConVar( "Supply_CrateDistance", "50" )
+local setHook   = false
 
-function drawProgressBar()
-	if width > fullWidth then
-		width = fullWidth;
+
+local function getEntityWidthLengthHeight( ent )
+	local min, max = ent:WorldSpaceAABB()
+	return max - min
+end
+
+
+local function drawProgressBar()
+	if width > fullWidth then width = fullWidth end
+
+	-- FIX: ScrW/ScrH must be called at draw time, not file scope
+	local sw = ScrW()
+	local sh = ScrH()
+	local x  = sw / 2 - fullWidth / 2
+	local y  = sh / 2 - height / 2
+
+	draw.SimpleText( "Capturing...", "MW2Font", sw / 2, sh / 2 - ( height + offset ), Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+	surface.SetDrawColor( 50, 50, 50, 150 )
+	surface.DrawRect( x - offset / 2, y - offset / 2, fullWidth + offset, height + offset )
+	surface.SetDrawColor( 255, 255, 255, 255 )
+	surface.DrawRect( x, y, width, height )
+
+	-- FIX: GetNetworkedBool -> GetNWBool; nil guard -> IsValid
+	if not IsValid( lpl ) or not lpl:GetNWBool( "SupplyCrate_DrawBarBool", false ) then
+		timer.Stop( "SupplyCrate_ProgressBarTimer" )
+		hook.Remove( "HUDPaint", "SupplyCrate_ProgressBar" )
+		hook.Remove( "Tick",     "Check_Player" )
+		return
 	end
-	draw.SimpleText("Capturing...", "MW2Font", ScrW()/2, ScrH()/2 - (height + offset), Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-	surface.SetDrawColor( 50, 50, 50, 150)
-	surface.DrawRect(x - offset/2, y - offset/2, fullWidth + offset, height + offset)	
-	surface.SetDrawColor( 255, 255, 255, 255)
-	surface.DrawRect(x, y, width, height)	
-	
-	
-	if !lpl:GetNetworkedBool("SupplyCrate_DrawBarBool")  then		
-		
-		
-		timer.Stop("SupplyCrate_ProgressBarTimer")
-		
-		
-		hook.Remove("HUDPaint", "SupplyCrate_ProgressBar");
-		
-		
-		hook.Remove( "Tick", "Check_Player" )  //  REMOVE CUSTOM HOOK CALLED:	"Check_Player"
-		
-		
-		return;
-	
-	
-	end	
+
 	if width >= fullWidth then
-		net.Start("SupplyCrate_GiveReward")
+		net.Start( "SupplyCrate_GiveReward" )
 		net.SendToServer()
-		--datastream.StreamToServer( "SupplyCrate_GiveReward" )
-		timer.Stop("SupplyCrate_ProgressBarTimer")
-		
-		
-		hook.Remove("HUDPaint", "SupplyCrate_ProgressBar");
-		
-		
-		hook.Remove("HUDPaint", "SupplyCrate_PopUpText")
-		
-		
-		hook.Remove( "Tick", "Check_Player" )	//  REMOVE CUSTOM HOOK CALLED:	"Check_Player"
-		
-		
-		return;
-	
-	
+		timer.Stop( "SupplyCrate_ProgressBarTimer" )
+		hook.Remove( "HUDPaint", "SupplyCrate_ProgressBar" )
+		hook.Remove( "HUDPaint", "SupplyCrate_PopUpText" )
+		hook.Remove( "Tick",     "Check_Player" )
 	end
-	
-	
 end
 
 
-function increment()
-	width = width + lpl:GetNetworkedFloat("SupplyCrate_Inc");
+local function increment()
+	if not IsValid( lpl ) then return end
+	-- FIX: GetNetworkedFloat -> GetNWFloat
+	width = width + lpl:GetNWFloat( "SupplyCrate_Inc", 0 )
 end
 
-function startProgressBar()
-	lpl = LocalPlayer();
+
+local function startProgressBar()
+	lpl   = LocalPlayer()
 	width = 0
-	hook.Add("HUDPaint", "SupplyCrate_ProgressBar", drawProgressBar)	
-	timer.Create("SupplyCrate_ProgressBarTimer", 0.01 , fullWidth, increment)
-	
-	
-end
-
-function setUp()
-	timer.Simple( 0.05, startProgressBar );
+	hook.Add( "HUDPaint", "SupplyCrate_ProgressBar", drawProgressBar )
+	timer.Create( "SupplyCrate_ProgressBarTimer", 0.01, fullWidth, increment )
 end
 
 
-usermessage.Hook( "SupplyCrate_DrawBar", setUp )
+local function setUp()
+	timer.Simple( 0.05, startProgressBar )
+end
 
 
-usermessage.Hook( "CHECK_PLAYER_INPUT", function()  //	IF THE SYSTEM DETECTS THAT A MESSAGE HAS BEEN SENT TO THE USER *AND* THAT MESSAGE IS SPECIFICALLY:	"CHECK_PLAYER_INPUT"  -  DO THE FOLLOWING...
+local function checkPlayerInput()
+	hook.Add( "Tick", "Check_Player", function()
+		if LocalPlayer():KeyDown( IN_USE ) then
+			net.Start( "START_CAPTURING" )
+			net.SendToServer()
+		else
+			net.Start( "STOP_CAPTURING" )
+			net.SendToServer()
+		end
+	end )
+end
 
 
-	hook.Add( "Tick", "Check_Player", function()	//	ADD A HOOK NAMED:	"Check_Player"	-	FOR EVERY "TICK" ON THE SYSTEM, DO THE FOLLOWING...
-
-
-		if LocalPlayer():KeyDown( IN_USE ) == true then  //  CHECK:  IF THE LOCAL PLAYER *HOLDS DOWN* THEIR "USE" KEY, THEN...
-	
-	
-			net.Start( "START_CAPTURING" )  //	BEGIN A (NETWORK) MESSAGE BLOCK:	NAME THE MESSAGE "START_CAPTURING"
-		
-		
-			net.SendToServer()  //	SEND THIS MESSAGE TO THE SERVER
-			
-			
-		else  //  IF THE LOCAL PLAYER IS *NOT HOLDING DOWN* THEIR "USE" KEY ( OR THEY *STOPPED HOLDING DOWN* THE KEY ), THEN...
-		
-		
-			net.Start( "STOP_CAPTURING" )  //	BEGIN A (NETWORK) MESSAGE BLOCK:	NAME THE MESSAGE "STOP_CAPTURING"
-			
-			
-			net.SendToServer()	//	SEND THIS MESSAGE TO THE SERVER
-		
-		
-		end  //  FINISH THE CHECK
-
-
-	end )  //  COMPLETE THE FUNCTION ( FOR THE NESTED HOOK )
-
-
-end )  //  COMPLETE THE FUNCTION ( FOR THE PARENT HOOK )
+-- FIX: usermessage.Hook x2 -> net.Receive
+net.Receive( "SupplyCrate_DrawBar",  setUp )
+net.Receive( "CHECK_PLAYER_INPUT",   checkPlayerInput )
 
 
 function ENT:Draw()
-	
-	
-	self.Entity:DrawModel()
-	
-	
-	local reward = self:GetNetworkedString( "SupplyCrate_Reward" )
-	
-	
-	local tex = Material( "vgui/killstreaks/animated/" .. reward )
-	
-	
+	self:DrawModel()
+
+	-- FIX: self:GetNetworkedString -> self:GetNWString
+	local reward = self:GetNWString( "SupplyCrate_Reward", "" )
+	if reward == "" then return end
+
+	local tex
 	if reward == "ammo" then
-
-
 		tex = Material( "vgui/killstreaks/ammo" )
-	
-	
+	else
+		tex = Material( "vgui/killstreaks/animated/" .. reward )
 	end
 
-	
-	local wlh = getEntityWidthLengthHeight(self)
-	local eHeight = wlh.z
-	local width = wlh.x
-	entPos = self:GetPos() + Vector(0, 0, eHeight + 8)
-	
-	cam.Start3D2D( entPos, Angle(0, LocalPlayer():GetAngles().y - 90, 90), 1 )
-        surface.SetMaterial( tex )
-		surface.SetDrawColor( 255, 255, 255 ) // Makes sure the image draws with all colors
-		surface.DrawTexturedRect(-8, -8, 16,16)
-    cam.End3D2D()
-	
-	local tab = ents.FindInSphere(self:GetPos(), DisFromCrate:GetInt())	
+	local wlh    = getEntityWidthLengthHeight( self )
+	local entPos = self:GetPos() + Vector( 0, 0, wlh.z + 8 )
 
-	if table.HasValue(tab,LocalPlayer()) && !setHook then
+	cam.Start3D2D( entPos, Angle( 0, LocalPlayer():GetAngles().y - 90, 90 ), 1 )
+		surface.SetMaterial( tex )
+		surface.SetDrawColor( 255, 255, 255 )
+		surface.DrawTexturedRect( -8, -8, 16, 16 )
+	cam.End3D2D()
+
+	local tab = ents.FindInSphere( self:GetPos(), DisFromCrate:GetInt() )
+	if table.HasValue( tab, LocalPlayer() ) and not setHook then
 		local function drawPopUpText()
-			local str = "a " .. reward;
-			if string.find(reward, "_") then
-				local sep = string.Explode("_", reward)
-				str = "a " .. sep[1] .. " " .. sep[2];
-			elseif reward == "ammo" then str = reward end			
-						
-			draw.SimpleText("Press and hold \"USE\" for " .. str, "MW2Font", ScrW()/2, ScrH()/2 + (height + offset), Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+			local str
+			if string.find( reward, "_" ) then
+				local sep = string.Explode( "_", reward )
+				str = "a " .. sep[1] .. " " .. sep[2]
+			elseif reward == "ammo" then
+				str = reward
+			else
+				str = "a " .. reward
+			end
+			draw.SimpleText( "Press and hold \"USE\" for " .. str, "MW2Font", ScrW() / 2, ScrH() / 2 + ( height + offset ), Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
 		end
-		hook.Add("HUDPaint", "SupplyCrate_PopUpText", drawPopUpText)
-		setHook = true;
-	elseif !table.HasValue(tab,LocalPlayer()) then
-		hook.Remove("HUDPaint", "SupplyCrate_PopUpText")
-		setHook = false;
+		hook.Add( "HUDPaint", "SupplyCrate_PopUpText", drawPopUpText )
+		setHook = true
+	elseif not table.HasValue( tab, LocalPlayer() ) then
+		hook.Remove( "HUDPaint", "SupplyCrate_PopUpText" )
+		setHook = false
 	end
-	
-end 
-
-function getEntityWidthLengthHeight(ent) --The function returns a vector with width as the vector's x, length as the vector's y, and height as the vector's z.
-    local min,max = ent:WorldSpaceAABB()
-    local offset=max-min
-    return offset
 end
